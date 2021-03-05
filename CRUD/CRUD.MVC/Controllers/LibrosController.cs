2defc949b18f33f946d12b318054bc8d59b848e6
Tomás Malvissi <tomasmalvissi@hotmail.com>
2021-03-05 19:15:31 -0300

    metodo http y view pages para editar

--- CRUD/CRUD.MVC/Controllers/LibrosController.cs
+++ CRUD/CRUD.MVC/Controllers/LibrosController.cs
@@ -21,9 +21,80 @@ namespace CRUD.MVC.Controllers
             return View(listLibros);
         }
 
+        //HTTP GET DEL CREATE PARA MOSTRAR LA VISTA
         public IActionResult Create()
         {
             return View();
+        }
+
+        //HTTP POST DEL CREATE PARA CREAR EL LIBRO
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libros.Add(libro);
+                _context.SaveChanges();
+
+                TempData["Mensaje"] = "Libro creado con exito.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //HTTP GET DEL EDIT PARA OBETENER EL ID
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var libro = _context.Libros.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+        //HTTP POST DEL EDIT PARA EDITAR EL LIBRO
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libros.Update(libro);
+                _context.SaveChanges();
+
+                TempData["Mensaje"] = "Libro actualizado con exito.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //HTTP GET DEL DELETE PARA OBETENER EL ID
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var libro = _context.Libros.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return View(libro);
         }
     }
 }
